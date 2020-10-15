4545c8a0cab7fa710516d95b5564bb83090954e5
zhassbala <besimbekovr@gmail.com>
2020-10-16 05:19:04 +0600

    week 2-3 hw almost done

--- LECTURE HOMEWORKS/WEEK 5/js/script.js
+++ LECTURE HOMEWORKS/WEEK 5/js/script.js
@@ -1,18 +1,24 @@
-function sayHello(raseke, smthelse){
-    if (prompt("Hello, what is your name?", "") == "Rassul"){
-        raseke();
-    }
-    else {
-        smthelse();
-    }
-}
+// function sayHello(raseke, smthelse){
+//     if (prompt("Hello, what is your name?", "") == "Rassul"){
+//         raseke();
+//     }
+//     else {
+//         smthelse();
+//     }
+// }
 
-function f1(){
-    alert("oh yeah");
-}
+// function f1(){
+//     alert("oh yeah");
+// }
 
-function f2(){
-    alert("Fuck uuuu");
-}
+// function f2(){
+//     alert("Fuck uuuu");
+// }
 
-sayHello(f1, f2)
+// sayHello(f1, f2)
+
+let array = [1, 2, 3, 4, 5, 6]
+
+for (let i in array){
+    console.log(i);
+}
